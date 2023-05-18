70347ddbb87a27c4079109b40a0a6b7d654ecc47
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-05-18 21:34:29 +0530

    Create Program_Ypur_Own_Calculator.c

---
+++ Program_Ypur_Own_Calculator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+    int a,b;
+    char c;
+    scanf("%d %d %c", &a, &b, &c);
+        switch(c){
+    	    case '+': printf("%d\n",a+b);
+    	              break;
+    	    case '-': printf("%d\n",a-b);
+                      break;
+    	    case '*': printf("%d\n",a*b);
+    	              break;
+    	    case '/': printf("%.7f\n",1.0*a/b);
+    	              break;
+    	}	
+	return 0;
+}
+
