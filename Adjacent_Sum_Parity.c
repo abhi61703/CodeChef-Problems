86a80ec3da016bba293fe8a2ade0856256134604
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-05-22 21:20:47 +0530

    Create Adjacent_Sum_Parity.c

---
+++ Adjacent_Sum_Parity.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int n;
+	    scanf("%d",&n);
+	    int a[n],count=0;
+	    for(int i=0;i<n;i++){
+	        scanf("%d",&a[i]);
+	    }
+	    for(int i=0;i<n;i++){
+	        if(a[i]==1){
+	            count++;
+	        }
+	    }
+	    if(count%2==0){
+	        printf("YES\n");
+	    }
+	    else{
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
+
