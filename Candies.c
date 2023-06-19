c04e755694d824f663d1a640de09c61b29dd567d
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-06-19 22:14:11 +0530

    Create Candies.c

---
+++ Candies.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int n;
+	    scanf("%d",&n);
+	    int a[2*n];
+	    for(int i=0;i<2*n;i++){
+	        scanf("%d",&a[i]);
+	    }
+	    int count;
+	    for(int i=0;i<2*n;i++){
+	        count=0;
+	        for(int j=i+1;j<2*n;j++){
+	            if(a[i]==a[j])
+	                count++;
+	        }
+	        if(count>1)
+	            break;
+	    }
+	    if(count>1)
+	        printf("No\n");
+	    else
+	        printf("Yes\n");
+	    
+	}
+	return 0;
+}
+
