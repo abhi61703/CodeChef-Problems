fc9efc993af0d03f46cc1ffe4134ff6d01d5f4e5
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-08-03 22:36:29 +0530

    Create Is_The_Score_Consistent.c

---
+++ Is_The_Score_Consistent.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int a,b,c,d;
+	    scanf("%d%d%d%d",&a,&b,&c,&d);
+	    (c<a || d<b)?printf("IMPOSSIBLE\n"):printf("POSSIBLE\n");
+	    
+	}
+	return 0;
+}
+
